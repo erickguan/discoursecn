5a8a9c6252ab01645bc9fb6e0c7eb6470a9f87c1
Erick Guan <fantasticfears@gmail.com>
2014-09-15 01:51:17 +0800

    pull resources

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,21 @@ module ApplicationHelper
       default
     end
   end
+
+  def video_tag(path, options)
+    if options[:size]
+      width = 1
+    else
+
+    end
+<<ss
+    <video width="420" height="200" poster="../video/replyread.png" autoplay="" loop="">
+    <source src="../video/reply_read_retina.mp4" type="video/mp4" media="(min-device-pixel-ratio:1.5), (-webkit-min-device-pixel-ratio:1.5), (min--moz-device-pixel-ratio:1.5), (-o-min-device-pixel-ratio:1.5)">
+    <source src="../video/reply_read_retina.ogg" type="video/ogg" media="(min-device-pixel-ratio:1.5), (-webkit-min-device-pixel-ratio:1.5), (min--moz-device-pixel-ratio:1.5), (-o-min-device-pixel-ratio:1.5)">
+    <source src="../video/reply_read.mp4" type="video/mp4">
+    <source src="../video/reply_read.ogg" type="video/ogg">
+        Your browser does not support the video tag.
+                                                    </video>
+ss
+  end
 end
