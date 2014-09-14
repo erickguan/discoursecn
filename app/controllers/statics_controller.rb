b867d820a1bf6703216d2298da533f7e93df53c1
Erick Guan <fantasticfears@gmail.com>
2014-09-15 02:11:22 +0800

    move resources

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -2,4 +2,12 @@ class StaticsController < ApplicationController
   def home
     render :home
   end
+
+  def about
+    render :about
+  end
+
+  def faq
+    render :faq
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'statics#home'
+
+  get '/about', to: 'statics#about'
+  get '/faq', to: 'statics#faq'
 end
