629fd018ed240bf5ca0ce0f531a0294c482f3ae8
Erick Guan <fantasticfears@gmail.com>
2015-07-01 09:55:08 +0800

    moving to jekyll

---
+++ README.md
@@ -0,0 +1,3 @@
+### Discourse 中文站
+
+是一个 discourse.org 的中文翻译。
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'statics#home', as: :home
-
-  get '/about', to: 'statics#about', as: :about
-  get '/about/why', to: 'statics#why', as: :why
-  get '/faq', to: 'statics#faq', as: :faq
-  get '/buy', to: 'statics#buy', as: :buy
-end
